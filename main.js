b53e85fee64e80e73873c5acbc08f58d134936b2
Zolmeister <zolikahan@gmail.com>
2014-03-08 18:10:37 -0800

    updated intro text

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 d()
-b.innerHTML += '<style>body{text-align:center}#o{font-size:40px;font-family:sans;border:1px solid; width:21rem;display:inline-block}</style><button id=p onclick=play()>Play</button><br><br><span id=o style="text-align:left;font-family:\'Ubuntu Mono\', mono">_</span><br><br><br><textarea rows=20 cols=100 id=i>12345678901234 speed read anything, while reading for speed. comprehension is key as anything can be read in a speedy way but still have great results. while text can be complex, reading may be greater for anything speed read is comprehended. feel free to read anything here to better grasp results comprehended</textarea><br>Speed (wpm): <input id=s value=500>';
+b.innerHTML += '<style>body{text-align:center}#o{font-size:40px;font-family:sans;border:1px solid; width:21rem;display:inline-block} #i{padding:20px;text-align:left;width:600px;height:300px;margin:0 auto;border:1px solid}</style><button id=p onclick=play()>Play</button><br><br><span id=o style="text-align:left;font-family:\'Ubuntu Mono\', mono">_</span><br><br><br><div id=i>Welcome to <a href="//zethos.zolmeister.com">Zethos!</a> <br><br>\
+Zethos is a speed reading tool, inspired by those such as Gritz, Sprtiz, and Spreeder. <br>\
+It\'s free and open source on <a href="//github.com/Zolmeister/Zethos">GitHub</a>. <br><br>\
+With Zethos, anyone can easily read at 500wpm instead of the average 250wpm. <br>\
+It works by keeping your eyes focused on one single point, the zethos-letter in red, to minimize the work your eye has do do while reading. Most reading time is spent on small eyemovements scanning across a \
+page, but by keeping everything in one place the reader can read more quickly and may even increase comprehention. <br><br>\
+Want to embed Zethos on your website? It\'s easy, just embed the code below, and set a global javascript variable named \'Z\'\ with the value of your text. Zethos will handle the rest. </div><br>Speed (wpm): <input id=s value=500>';
 
-
+i.contentEditable = true
 playing = false
-
 function hyphenate(str) {
-  if (str.lenth <= 7) {
+  if (str.length <= 7) {
     return str
   }
   if (str.length <= 10) {
@@ -26,7 +31,7 @@ function getFocus(str) {
   }
   return focus
 }
-function parse(string) {
+function parse(str) {
   // Logic
   // strings will be broken out into words
   // find the focus point of the word
@@ -49,7 +54,7 @@ function parse(string) {
   //    return {7},hyphenated{x}
   
   // return 2d array with word and focus point
-  return string.split(' ').reduce(function(words, word) {
+  return str.trim().split(/[\s\n]+/).reduce(function(words, word) {
     // focus point
     var focus = getFocus(word);
     if ( word.length >= 15 || word.length - focus > 7) {
@@ -71,7 +76,7 @@ function repeat(s, n) {
 
 function play() {
   index = 0
-  input = parse(i.value);
+  input = parse(i.textContent);
   if(playing) {
      playing = false
      p.innerHTML = 'Play'
@@ -86,22 +91,19 @@ function play() {
       o.innerHTML = repeat('&nbsp;', 7 - w[1])+w[0].slice(0,w[1])+'<span style="color:red;">'+w[0][w[1]]+'</span>'+w[0].slice(w[1]+1)
       p.innerHTML = 'Stop'
       index++
-      var t = 60000/s.value
-      /*
-      $next_shot += ($timeout / 4 ) * ($word_length - 6) if ($word_length > 6);
-	$next_shot += $timeout / 2 if ($word =~ /.*,$/);
-	$next_shot += $timeout * 1.5 if ($word =~ /.*[\.!\?;]«?$/);
-    */
       
-      if (w[0].length > 6) {
+      var t = 60000/s.value
+      
+      if (w[0].length > 6)
         t+=t/4
-      }
-      if(w[0].indexOf(',')!==-1) {
+        
+      if (~w[0].indexOf(','))
         t+=t/2
-      }
+        
       if(/[\.!\?;]$/.test(w[0])) {
         t+= t*1.5
       }
+      
       setTimeout(loop, t)
     }
   })()  
