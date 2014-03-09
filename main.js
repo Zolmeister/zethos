66de69e39095efafcd5f171cc9ad1067692f0ecf
Zolmeister <zolikahan@gmail.com>
2014-03-08 21:56:42 -0800

    realized that I cant actually fit that much text

--- main.js
+++ main.js
@@ -1,37 +1,8 @@
 d()
-b.innerHTML += '<style>body{text-align:center}#o{font-size:40px;font-family:sans;border:1px solid; width:21rem;display:inline-block} #i{padding:20px;text-align:left;width:600px;height:300px;margin:0 auto;border:1px solid}</style><button id=p onclick=play()>Play</button><br><br><span id=o style="text-align:left;font-family:\'Ubuntu Mono\', mono">_</span><br><br><br><div id=i>Welcome to <a href="//zethos.zolmeister.com">Zethos!</a> <br><br>\
-Zethos is a speed reading tool, inspired by those such as Gritz, Sprtiz, and Spreeder. <br>\
-It\'s free and open source on <a href="//github.com/Zolmeister/Zethos">GitHub</a>. <br><br>\
-With Zethos, anyone can easily read at 500wpm instead of the average 250wpm. <br>\
-It works by keeping your eyes focused on one single point, the zethos-letter in red, to minimize the work your eye has do do while reading. Most reading time is spent on small eyemovements scanning across a \
-page, but by keeping everything in one place the reader can read more quickly and may even increase comprehention. <br><br>\
-Want to embed Zethos on your website? It\'s easy, just embed the code below, and set a global javascript variable named \'Z\'\ with the value of your text. Zethos will handle the rest. </div><br>Speed (wpm): <input id=s value=500>';
+b.innerHTML += "<style>body{width:600px;margin:0 auto;font-family:Courier,monospace}#o{font-size:40px;border:2px solid;width:21rem;display:inline-block}#i{resize:both;overflow:auto;padding:20px;width:600px;height:250px;margin:0 auto;border:1px solid}v{color:red}</style><br><button id=x onclick=p()>Play</button><br><br><div id=o>.</div><br><br><br><div contenteditable id=i>Welcome to <a contenteditable=false href=//zethos.zolmeister.com>Zethos!</a> <br><br>This is a speed reading tool, inspired by others such as Gritz, Sprtiz, and Spreeder. <br>It's free and open source on <a contenteditable=false href=//github.com/Zolmeister/Zethos>GitHub.";
 
-i.contentEditable = true
-playing = false
-function hyphenate(str) {
-  if (str.length <= 7) {
-    return str
-  }
-  if (str.length <= 10) {
-    return str.slice(0,str.length - 3)+'-'+str.slice(str.length-3)
-  }
-  if (str.length <= 14) {
-    return str.slice(0,7)+'-'+str.slice(7)
-  }
-  return str.slice(0,7)+'-'+hyphenate(str.slice(7))
-}
-function getFocus(str) {
-  var focus = ~~(str.length/2)
-  for(var i=focus;i>=0;i--){
-    if ('aeiou'.indexOf(str[i]) !== -1){
-      focus = i
-      break
-    }
-  }
-  return focus
-}
-function parse(str) {
+var playing,index,input,loop,parse,focus,hyphenate;
+parse = function(str) {
   // Logic
   // strings will be broken out into words
   // find the focus point of the word
@@ -53,58 +24,56 @@ function parse(str) {
   //    else
   //    return {7},hyphenated{x}
   
+  hyphenate = function(str) {
+    return str.length <= 7 ? str : str.length <= 10 ? str.slice(0,str.length - 3)+'- '+str.slice(str.length-3) : str.slice(0,7)+'- '+hyphenate(str.slice(7))
+  }
+  
   // return 2d array with word and focus point
-  return str.trim().split(/[\s\n]+/).reduce(function(words, word) {
+  return str.trim().split(/[\s\n]+/).reduce(function(words, str) {
     // focus point
-    var focus = getFocus(word);
-    if ( word.length >= 15 || word.length - focus > 7) {
-      words = words.concat(parse(hyphenate(word).replace(/-/g,'- ')))
-    } else {
-      words.push([word, focus])
-    }
-    return words
+    focus = (str.length-1)/2|0
+    
+    for(j=focus;j>=0;j--)
+      if (/[aeiou]/.test(str[j])) {
+        focus = j
+        break
+      }
+    
+    t = 60000/500 // 500 wpm
+      
+    if (str.length > 6)
+      t+=t/4
+      
+    if (~str.indexOf(','))
+      t+=t/2
+      
+    if(/[\.!\?;]$/.test(str))
+      t+= t*1.5
+    
+    return str.length >= 15 || str.length - focus > 7 ? words.concat(parse(hyphenate(str))) : words.concat([[str, focus, t]])
   }, [])
 }
 
-function repeat(s, n) {
-  var o = '';
-  for(var i=0;i<n;i++){
-    o+=s
-  }
-  return o
-}
-
-function play() {
+p = function() {
   index = 0
   input = parse(i.textContent);
   if(playing) {
-     playing = false
-     p.innerHTML = 'Play'
-     return
+     playing = 0
+     return x.innerHTML = 'Play'
   }
-  playing = true;
-  
-  (function loop() {
-    if(playing) {
-      if(!input[index]) return p.innerHTML = 'Play'
-      var w = input[index]
-      o.innerHTML = repeat('&nbsp;', 7 - w[1])+w[0].slice(0,w[1])+'<span style="color:red;">'+w[0][w[1]]+'</span>'+w[0].slice(w[1]+1)
-      p.innerHTML = 'Stop'
-      index++
-      
-      var t = 60000/s.value
-      
-      if (w[0].length > 6)
-        t+=t/4
-        
-      if (~w[0].indexOf(','))
-        t+=t/2
-        
-      if(/[\.!\?;]$/.test(w[0])) {
-        t+= t*1.5
-      }
-      
-      setTimeout(loop, t)
-    }
-  })()  
+  playing = 1;
+  loop()
 }
+
+loop = function() {
+  if(playing) {
+      w = input[index++]
+      if(!t) return p()
+      
+      o.innerHTML = Array(8 - w[1]).join('&nbsp;')+w[0].slice(0,w[1])+'<v>'+w[0][w[1]]+'</v>'+w[0].slice(w[1]+1)
+      x.innerHTML = 'Stop'
+      setTimeout(loop, w[2])
+    }
+}
+
+p()
