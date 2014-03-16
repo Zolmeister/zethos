c382768168b8c3347abbf10703ad27e20c26dc6d
Zolmeister <zolikahan@gmail.com>
2014-03-15 22:19:35 -0700

    Improved newline support, thanks to #6

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 d()
 b.innerHTML += "<style>body{width:600px;margin:0 auto;font-family:Courier}#o{font-size:40px;border:2px solid;width:21rem;display:inline-block}#i{overflow:auto;padding:20px;width:600px;height:350px;border:1px solid}v{color:red}</style><br><br><button onclick=p()>Play</button><br><br><c id=o>.</c><br><br><p contenteditable id=i>Welcome to <a contenteditable=false href=//zethos.Zolmeister.com>Zethos!</a> <br><br>This is a speed reading tool inspired by Spritz ($3.5mil). <br>It's free and open source on <a contenteditable=false href=//github.com/Zolmeister/Zethos>GitHub.</a><br>-<a contenteditable=false href=//Zolmeister.com>Zolmeister";
 
-var playing,index,input,loop,parse,focus,hyphenate;
-parse = function(str) {
+var playing,index,loop,parse,focus,hyphenate,str,words;
+parse = function(words, str) {
   // Logic
   // strings will be broken out into words
   // find the focus point of the word
@@ -24,18 +24,16 @@ parse = function(str) {
   //    else
   //    return {7},hyphenated{x}
   
-  hyphenate = function(str) {
-    with(str)
-    return length <= 7 ? str : length <= 10 ? slice(0,length - 3)+'- '+slice(length-3) : slice(0,7)+'- '+hyphenate(slice(7))
+  hyphenate = function(words, str) {
+    with(words)
+    return length < 8 ? words : length < 11 ? slice(0,length - 3)+'- '+slice(length-3) : slice(0,7)+'- '+hyphenate(slice(7))
   }
   
   // return 2d array with word and focus point
-  return str.trim().split(/[\s\n]+/).reduce(function(words, str) {
+  return words.trim().replace(/([.?!])([A-Z-])/g, '$1 $2').split(/\s+/).reduce(function(words, str) {
     with(str) {
       // focus point
-      focus = (length-1)/2|0
-      
-      for(j=focus;j>=0;j--)
+      for(j=focus=(length-1)/2|0;j>=0;j--)
         if (/[aeiou]/.test(str[j])) {
           focus = j
           break
@@ -44,28 +42,27 @@ parse = function(str) {
       t = 60000/500 // 500 wpm
         
       if (length > 6)
-        t+=t/4
+        t += t/4
         
       if (~indexOf(','))
-        t+=t/2
+        t += t/2
         
-      if(/[\.!\?;]$/.test(str))
+      if(/[.?!]/.test(str))
         t+= t*1.5
       
-      return length >= 15 || length - focus > 7 ? words.concat(parse(hyphenate(str))) : words.concat([[str, focus, t]])
+      return length > 14 || length - focus > 7 ? words.concat(parse(hyphenate(str))) : words.concat([[str, focus, t]])
     }
   }, [])
 }
 
-p = function() {
+p = function(words,str) {
   index = 0
-  input = parse(i.textContent);
   playing = !playing
   playing && loop()
 }
 
-loop = function() {
-  w = input[index++] || p()
+loop = function(words,str) {
+  w = parse(i.textContent)[index++] || p()
   o.innerHTML = Array(8 - w[1]).join('&nbsp;')+w[0].slice(0,w[1])+'<v>'+w[0][w[1]]+'</v>'+w[0].slice(w[1]+1)
   playing && setTimeout(loop, w[2])
 }
